8813f80625f454e1b75fe55bcc5302b9482ee636
soham <sg5067@srmist.edu.in>
2026-02-19 09:13:33 +0530

    Initial commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -12,5 +12,9 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the Palindrome Checker Management System");
         System.out.println("Version : 1.0");
         System.out.println("System intialised successfully");
+
+
+
+
     }
 }
