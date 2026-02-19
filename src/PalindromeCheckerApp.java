91a97d001174471909611d0c85894f7ff0508ce1
soham <sg5067@srmist.edu.in>
2026-02-19 10:31:04 +0530

    give proper commit

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,25 +4,23 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-
+        // Hardcoded input string
         String input = "madam";
 
-        boolean isPalindrome = true;
+        // Variable to store reversed string
+        String reversed = "";
 
-
-        for (int i = 0; i < input.length() / 2; i++) {
-
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        // Iterate from the last character to the first
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
 
-
-        if (isPalindrome) {
+        // Check if original equals reversed
+        if (input.equals(reversed)) {
             System.out.println(input + " is a Palindrome");
         } else {
             System.out.println(input + " is NOT a Palindrome");
         }
+
     }
 }
